5dcc8064001991fc840a4292cfd665fab0710a8d
Bucky Kittinger <larrykittinger@gmail.com>
2019-01-15 21:54:54 -0500

    Version 1.5.0

--- eosio.cdt.rb
+++ eosio.cdt.rb
@@ -2,8 +2,8 @@ class EosioCdt < Formula
 
    homepage "https://github.com/eosio/eosio.cdt"
    revision 0
-   url "https://github.com/eosio/eosio.cdt/archive/v1.4.1.tar.gz"
-   version "1.4.1"
+   url "https://github.com/eosio/eosio.cdt/archive/v1.5.0.tar.gz"
+   version "1.5.0"
    
    option :universal
 
@@ -21,9 +21,9 @@ class EosioCdt < Formula
    depends_on :arch =>  :intel
   
    bottle do
-      root_url "https://github.com/eosio/eosio.cdt/releases/download/v1.4.1"
-      sha256 "a6c87970c654e5019729ad2184868d2e8940e0d0caac2b3d145080b54a6e445f" => :high_sierra
-      sha256 "2a9db35ff00e0246e75b925b06c82d35d7dd6dfd1eba745ae83ffd62ccd55084" => :mojave
+      root_url "https://github.com/eosio/eosio.cdt/releases/download/v1.5.0"
+      sha256 "a4db73906ea5cf7d7113a7127bec8b67e4eb37412a7cca1abe4aa8a4ca1e8576" => :high_sierra
+      sha256 "b8a0b7e964e30dc79a44b19cbef510c5a65309bdcb4fc99b69360fccc80e1d1d" => :mojave
    end
    def install
       raise "Error, only supporting binary packages at this time"
