4e196e9429cdad35b0895b9216f5df4915ffac5d
EOSIO Automation <36459142+eosio-cicd@users.noreply.github.com>
2021-07-12 20:33:11 -0400

    Update brew file for new release

--- eosio.cdt.rb
+++ eosio.cdt.rb
@@ -1,31 +1,34 @@
 class EosioCdt < Formula
-  homepage "https://github.com/eosio/eosio.cdt"
-  url "https://github.com/eosio/eosio.cdt/archive/v1.8.0.tar.gz"
-  version "1.8.0"
-  revision 0
+   # typed: false
+   # frozen_string_literal: true
 
-  bottle do
-    root_url "https://github.com/eosio/eosio.cdt/releases/download/v1.8.0"
-    sha256 catalina: "1af9ec47abaa2c4e3cc7037a18d4cd8b57dea2a018b7868fd2d2351daf89045b"
-    sha256 mojave:   "9489660420c4582fd3520fefb9226b56ef9610ddaa604d3c1974dad86b1424e9"
-  end
+   homepage "https://github.com/eosio/eosio.cdt"
+   revision 0
+   url "https://github.com/eosio/eosio.cdt/archive/v1.8.1.tar.gz"
+   version "1.8.1"
+   
+   option :universal
 
-  option :universal
-
-  depends_on "automake" => :build
-  depends_on "cmake" => :build
-  depends_on "doxygen" => :build
-  depends_on "gettext" => :build
-  depends_on "gmp" => :build
-  depends_on "graphviz" => :build
-  depends_on "lcov" => :build
-  depends_on "libtool" => :build
-  depends_on "wget" => :build
-  depends_on xcode: :build
-  depends_on arch: :intel
-  depends_on macos: :mojave
-  def install
-    raise "Error, only supporting binary packages at this time"
-  end
+   depends_on "cmake" => :build
+   depends_on "automake" => :build
+   depends_on "libtool" => :build
+   depends_on "wget" => :build
+   depends_on "gmp" => :build
+   depends_on "gettext" => :build
+   depends_on "doxygen" => :build
+   depends_on "graphviz" => :build
+   depends_on "lcov" => :build
+   depends_on xcode: :build
+   depends_on macos: :mojave
+   depends_on arch: :intel
+  
+   bottle do
+      root_url "https://github.com/eosio/eosio.cdt/releases/download/v1.8.1"
+      sha256 mojave: "b39fa0e2c86217b86f3b1e4a2b7949855cff7421294e054ee4b320dc95029ea3"
+      sha256 catalina: "b70a06f06c2c72480a468092a6559680180b6450869c946999d484357d7245d9"
+   end
+   def install
+      raise "Error, only supporting binary packages at this time"
+   end
 end
 __END__
